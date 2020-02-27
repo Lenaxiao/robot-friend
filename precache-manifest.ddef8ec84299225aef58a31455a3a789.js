6bdafbc10c585ef5878f793a83c3417a3c45234a
Sijia Xiao <lenaxiao@uw.edu>
2020-02-26 16:17:22 -0800

    Updates

---
+++ precache-manifest.ddef8ec84299225aef58a31455a3a789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b623e26e879e6466681d69e84be30a5",
+    "url": "/robot-friend/index.html"
+  },
+  {
+    "revision": "9d21efe1acd8cd7d3676",
+    "url": "/robot-friend/static/css/main.f05da0f3.chunk.css"
+  },
+  {
+    "revision": "0564a70d837b303f1c69",
+    "url": "/robot-friend/static/js/2.1fadb90e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robot-friend/static/js/2.1fadb90e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d21efe1acd8cd7d3676",
+    "url": "/robot-friend/static/js/main.6fc9cadb.chunk.js"
+  },
+  {
+    "revision": "2fa7542bbe534d22161e",
+    "url": "/robot-friend/static/js/runtime-main.23d0a7e8.js"
+  }
+]);
